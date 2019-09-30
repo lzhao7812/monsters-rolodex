975370f4650dd8e6abb10d75d18c50ba996911d3
Zhao <ZHAOL@dfo-mpo.ca>
2019-09-30 14:52:14 -0400

    Updates

---
+++ precache-manifest.98076e16a317c38c4e378785bac294a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "810915d2455bfac57a003167e8c6002b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d4dca3ee080fa06bd72d",
+    "url": "/monsters-rolodex/static/css/main.ad31053c.chunk.css"
+  },
+  {
+    "revision": "40d859b7a8ceba360fc0",
+    "url": "/monsters-rolodex/static/js/2.c626c3f2.chunk.js"
+  },
+  {
+    "revision": "d4dca3ee080fa06bd72d",
+    "url": "/monsters-rolodex/static/js/main.68fd1987.chunk.js"
+  },
+  {
+    "revision": "460532cfc11f35cfb4de",
+    "url": "/monsters-rolodex/static/js/runtime-main.6571bd3c.js"
+  }
+]);
